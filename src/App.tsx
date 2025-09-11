e878bea2952095a093829af8049ce026c619d53e
Shin <sirapop1998@gmail.com>
2025-09-11 13:26:16 +0700

    feat: installed shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button"
 
 
 function App() {
@@ -8,6 +9,9 @@ function App() {
       <section>
         <h1>My First React App on Vite</h1>
       </section>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
