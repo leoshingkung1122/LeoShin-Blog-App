063244749f4c25eefb2da432cc00ccbaf06ff193
Shin <sirapop1998@gmail.com>
2025-09-11 14:39:53 +0700

    feat: implement NavBar and HeroSection components in App

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,23 @@
+function NavBar() {
+    return (
+      <nav className="flex items-center justify-between py-4 px-8 border-b bg-[#F9F8F6]">
+        <a href="/" className="text-2xl font-bold">
+          LeoShin<span className="text-green-500">.</span>
+        </a>
+        <div className="hidden md:flex space-x-4">
+          <a href="/login" className="px-9 py-2  rounded-full border">
+            Log in
+          </a>
+          <a
+            href="/signup"
+            className="px-8 py-2 bg-gray-900 text-white rounded-full hover:bg-gray-700 transition-colors"
+          >
+            Sign up
+          </a>
+        </div>
+        <button className="md:hidden">Menu</button>
+      </nav>
+    );
+  }
+
+export default NavBar;
