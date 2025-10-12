d1be24462ed8b8e0da5f815c4b8a07f06e78e938
Shin <sirapop1998@gmail.com>
2025-10-12 20:10:16 +0700

    feat: implement authentication context and routes; add ProtectedRoute and AuthenticationRoute components for user access control; update App and main files for routing integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import HomePage from "./pages/HomePage";
 import ViewPostPage from "./pages/ViewPostPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
+import { useAuth } from "@/contexts/authentication";
+import jwtInterceptor from "./utils/jwtinterceptor";
+import ProtectedRoute from "./components/auth/ProtectRoute";
+import AuthenticationRoute from "./components/auth/AuthenticationRoute";
+
+jwtInterceptor();
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/post/:postId" element={<ViewPostPage />} />
-          <Route path="/404" element={<NotFoundPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/post/:postId" element={<ViewPostPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
       <Toaster
         toastOptions={{
           unstyled: true,
